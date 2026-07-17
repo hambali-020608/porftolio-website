0784e3a106c12528ea793ae5a0a50ce080ff0af2
hambali-020608 <subastianhambali@gmail.com>
2026-07-17 21:58:52 +0700

    feat(remove credential)

--- src/lib/firebase_admin.js
+++ src/lib/firebase_admin.js
@@ -1,7 +1,6 @@
 
 import { initializeApp, getApps, cert } from "firebase-admin/app";
 import { getFirestore } from "firebase-admin/firestore";
-import serviceAccount from '../../firebase-credential.json'
 
 const apps = getApps()  
 
